a7a16b037a499aff9a4f574f7a128026879e7188
soheeeeP <tpfj@naver.com>
2022-01-07 01:17:26 +0900

    #6 ADD: 설정 menu에 command값 지정 & spinbox 탬플릿 초안 생성

--- window.py
+++ window.py
@@ -1,11 +1,15 @@
 from tkinter import *
 
 
+# TODO: Window, Menu, Frame 별도의 class로 분리하기
 class Window(object):
     def __init__(self, db):
         self.db = db
         self._window = None
         self._menu = None
+
+        self.width = 720
+        self.height = 540
 
     def __del__(self):
         if self._window:
@@ -21,10 +25,9 @@ class Window(object):
         self._window = Tk()
         self._window.title(title)
 
-        width, height = 720, 540
-        start_w = int(self._window.winfo_screenwidth() - width) / 2
-        start_h = int(self._window.winfo_screenheight() - height) / 2
-        self._window.geometry(f"{width}x{height}+{int(start_w)}+{int(start_h)}")
+        start_w = int(self._window.winfo_screenwidth() - self.width) / 2
+        start_h = int(self._window.winfo_screenheight() - self.height) / 2
+        self._window.geometry(f"{self.width}x{self.height}+{int(start_w)}+{int(start_h)}")
         self._window.resizable(False, False)
 
     @property
@@ -46,11 +49,11 @@ class Window(object):
 
     def main_option_menu(self) -> Menu:
         menu = Menu(self._menu, tearoff=0)
-        menu.add_command(label='시간당 근무 인원수 설정')
+        menu.add_command(label='시간당 근무 인원수 설정', command=lambda: self.worker_per_term_frame())
         menu.add_separator()
-        menu.add_command(label='사수/부사수 옵션 설정')
+        menu.add_command(label='사수/부사수 옵션 설정', command=lambda: self.assistant_mode_on_off_frame())
         menu.add_separator()
-        menu.add_command(label='근무 교대 텀 설정')
+        menu.add_command(label='근무 교대 텀 설정', command=lambda: self.work_shift_term_frame())
         return menu
 
     def db_menu(self) -> Menu:
@@ -81,3 +84,45 @@ class Window(object):
         self._menu.add_cascade(label='DB 생성/조회', menu=self.db_menu())
         self._menu.add_cascade(label='추가사항', menu=self.exceptional_option_menu())
         self._menu.add_cascade(label='저장', menu=self.save_menu())
+
+    def spinbox_value_validator(self, value):
+        print(value)
+        if value.isdigit() and 0 <= int(value) <= 5:
+            return True
+        return False
+
+    def spinbox_frame(self, message, min_range, max_range):
+        frame = Toplevel()
+        frame.geometry(
+            f"{int(self.width / 2)}x{int(self.height / 2)}"
+            f"+{self._window.winfo_x() + int(self.width / 4)}+{self._window.winfo_y() + int(self.height / 4)}"
+        )
+        label = Label(frame, text=message, height=4).pack()
+
+        validate_command = (frame.register(self.spinbox_value_validator), "%P")
+        spinbox = Spinbox(
+            frame,
+            from_=min_range,
+            to=max_range,
+            validate='all',
+            validatecommand=validate_command
+        )
+        spinbox.pack()
+
+        # TODO: db연동하여 입력받은 옵션값 저장하기
+        Button(frame, text='저장하기', command=frame.destroy).pack()
+
+    def radiobutton_frame(self):
+        pass
+
+    def worker_per_term_frame(self):
+        # TODO: spinbox frame에 default로 db의 worker_per_term값이 뜨도록 설정
+        self.spinbox_frame(message="시간당 근무 인원 수(0 ~ 5)를 입력해주세요", min_range=0, max_range=5)
+
+    def assistant_mode_on_off_frame(self):
+        # TODO: radio frame에 default로 db의 assistant_mode가 체크되어 있도록 설정
+        self.radiobutton_frame()
+
+    def work_shift_term_frame(self):
+        # TODO: spinbox frame에 default로 db의 term_count값이 뜨도록 설정
+        self.spinbox_frame(message="근무 교대 횟수(0 ~ 24)를 입력해주세요", min_range=0, max_range=24)
