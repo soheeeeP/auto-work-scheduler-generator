9d63821153b7d0671d31647b240b6290191a9ffd
soheeeeP <tpfj@naver.com>
2022-01-08 17:43:53 +0900

    #11 ADD: user interface 생성

--- domain/interface/user.py
+++ domain/interface/user.py
@@ -1,57 +1,72 @@
+from typing import List, Tuple, TypeVar, NewType, Union
 from abc import ABCMeta, abstractmethod
 
 
+class UserType(object):
+    # UserInfo = NewType('UserInfo', Tuple[str])
+    # UserWeekDayTerm = NewType('UserDayTerm', Tuple[bool])
+    # UserHolidayTerm = NewType('UserHolidayTerm', Tuple[bool])
+    UserData = NewType('UserData', List[Tuple])
+
+
 class UserRepository(metaclass=ABCMeta):
+    def __init__(self):
+        self.query = None
+
+    @property
+    def query(self):
+        return self._query
+
+    @query.setter
+    def query(self, value):
+        self._query = value
+
     @abstractmethod
     def create_default_user_table(self):
         pass
 
     @abstractmethod
-    def add_term_related_columns_in_user(self):
+    def add_term_related_columns_in_user(self, term: int):
         pass
 
     @abstractmethod
-    def drop_term_related_columns_in_user(self):
+    def get_all_users(self) -> Union[List[List[Tuple]], None]:
         pass
 
     @abstractmethod
-    def get_all_users(self):
+    def get_user_by_name(self, name: str) -> Tuple:
         pass
 
     @abstractmethod
-    def get_user_by_name(self):
+    def set_user_name(self, name: str):
         pass
 
     @abstractmethod
-    def set_user_name(self):
+    def get_users_by_rank(self, rank: str) -> List[UserType.UserData]:
         pass
 
     @abstractmethod
-    def get_user_by_rank(self):
+    def set_user_rank(self, rank: str):
         pass
 
     @abstractmethod
-    def set_user_rank(self):
+    def get_users_by_status(self, status: str) -> List[UserType.UserData]:
         pass
 
     @abstractmethod
-    def get_user_by_status(self):
+    def set_user_status(self, status: str):
         pass
 
     @abstractmethod
-    def set_user_status(self):
+    def insert_new_user(self, data: List[UserType.UserData]):
         pass
 
     @abstractmethod
-    def insert_new_user(self):
+    def insert_dummy_users(self, data: List[UserType.UserData]):
         pass
 
     @abstractmethod
-    def insert_dummy_users(self):
-        pass
-
-    @abstractmethod
-    def delete_user(self):
+    def delete_user(self, user_id: int):
         pass
 
     @abstractmethod
